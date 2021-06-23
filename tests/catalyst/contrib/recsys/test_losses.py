948e93ad49b850afe06e380ce002b7cb4aa8ff8e
Philipp <casyfill@gmail.com>
2021-06-23 11:09:16 -0400

    brp_and_test

---
+++ tests/catalyst/contrib/recsys/test_losses.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+import pytest
+import torch
+# from catalyst.data import AllTripletsSampler
+
+ratings = {
+    0.6931: (torch.zeros(100), torch.zeros(100)),  # log(0.5 + loss.gamma)
+    0.6931: (torch.tensor([1000,2000]), torch.tensor([1000,2000])),  # log(0.5 + loss.gamma)
+    0.3133: (torch.ones(1000), torch.zeros(1000)),
+    0.1269: (torch.ones(1000), -torch.ones(1000)),
+}
+
+
+@pytest.mark.parametrize("answer,args", ratings.items())
+def test_BRPloss(answer, args):
+    """@TODO: Docs. Contribution is welcome."""
+    from catalyst.contrib.recsys.loss import BPRLoss
+    loss = BPRLoss()
+
+    assert float(loss.forward(*args)) == pytest.approx(answer, 0.001)
+    
+
+
